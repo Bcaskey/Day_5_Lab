924bf3f80f16f38918bc5808d3af32eaee0f6741
Bcaskey <bcaskeyu@charter.net>
2016-02-27 16:58:22 -0600

    appending array to words div. Need to fix array to append one item at a time.

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 // Set the lower case keyboard onload
 $(document).ready(function(){
+    
+    //Put this into the words div
+    var sentences = [
+        //"ten ate neite ate nee enet ite ate inet ent eate", 
+        //"Too ato too nOt enot one totA not anot tOO aNot",
+        //"oat itain oat tain nate eate tea anne inant nean",
+        //"itant eate anot eat nato inate eat anot tain eat",
+        //"nee ene ate ite tent tiet ent ine ene ete ene ate"
+        "text 1 ",
+        "text 2 ",
+        "text 3 ",
+        "text 4 ",
+        "text 5 "
+    ];
+    
+    
+    
+    //jQuery.each( arr, function( i, val ) {
+  //$( "#" + val ).text( "Mine is " + val + "." );
+    
+    
+    
+    $("#words").append(sentences);
+    
      $("#keyboard-upper-container").hide();
     // When shift key is down, show the upper case keyboard
         $(document).keydown(function (e){  //keydown understands the key, but not the optional values of the key shift/noshift

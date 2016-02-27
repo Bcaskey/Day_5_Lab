cc55c8f9703c117b8420a210be0e685da2bb79a3
Bcaskey <bcaskeyu@charter.net>
2016-02-27 14:28:05 -0600

    updated to print characters rather than ASCII

--- script.js
+++ script.js
@@ -16,55 +16,21 @@ $(document).ready(function(){
             }
         });
         
-        // below finds a keystroke value, from here I need to convert the ascii 
-        //code to text for the typed string line and attach a glyph
-        $(document).keypress(function (e){
-            var keycode = e.which;
-            var letter = e.innerHTML;
-                $("#words-typed").text(e.which); //keypress understand shift/no shift
-                // lower f is 102 upper f is 70, keycode is 70 for both, ascii key code is 102 and 70
-                console.log(letter);
-                //$("next-letter").text("this is a test");
-                // $("#next-letter").text($(this).text()); this passes the entire keyboard content
-
-        });
-        // lets taget the next letters tag <div class='row col-lg-12 text-center' id='next-letter'>letters typed</div>
 
             
-        
-    
+            $(document).on("keypress", function(e){
+                var letterCode = e.which;
+                $("#words-typed").text(String.fromCharCode(letterCode)); // now I need to make this a sentence with glyphs
+                $("#next-letter").text(String.fromCharCode(letterCode)); //I think this is the highlighted letter pressed key
+                // console.log(test);
+                // $("next-letter").text($(this).text());
+            });
+
+
+
 
 // end of original script tag below
-});
-
-
-//      ------      here is a sample key delete later
-        //   		<span class='well well-lg key' id='R'>R</span>
-        
-        
-        //This works, but it only saves one letter at a time. Delete when fixed above
-        // $(document).keydown(function (e){ 
-            // $("#words-typed").text(e.which);
-        // });
-        // Delete above, when words keep characters.
-
- 
-//     sample
-//     eventListener keypresss => function(e) {
-//         console.log(which);
-//         
-//         sample
-//     }
-//     
-//     sample
-//        button.addEventListener('click', doSomething); do something runs a function
-//        function doSomething(e) {
-//            var clickedElem = e.target; //stores e.target of click
-//            e.target = //whatever button you clicked
-//        }
-        
-//   sample
-   
+});   
      
 
 

8ef04e0557a97bfe9add3f5a56a3cb3c6a3c002a
Bcaskey <bcaskeyu@charter.net>
2016-02-29 20:02:42 -0600

    Found first letter of array

--- script.js
+++ script.js
@@ -3,29 +3,19 @@ $(document).ready(function(){
 
     // below working thru array by line
     // working on going through a loop on top of a loop
-
     var sentences = [
-        "first ", //"ten ate neite ate nee enet ite ate inet ent eate", 
-        "second ", //"Too ato too nOt enot one totA not anot tOO aNot",
-        "third ", //"oat itain oat tain nate eate tea anne inant nean",
-        "fourth ", //"itant eate anot eat nato inate eat anot tain eat",
-        "last " //"nee ene ate ite tent tiet ent ine ene ete ene ate"
+        "ten ate neite ate nee enet ite ate inet ent eate", 
+        "Too ato too nOt enot one totA not anot tOO aNot",
+        "oat itain oat tain nate eate tea anne inant nean",
+        "itant eate anot eat nato inate eat anot tain eat",
+        "nee ene ate ite tent tiet ent ine ene ete ene ate"
         ];
-    var letterCount = 0; //setting at zero before keyUp or keyPress
-    var currentLetter;    
-    var line1 = sentences[0];
-    var line2 = sentences[1];
-    var line3 = sentences[2];
-    var line4 = sentences[3];        
-    var line5 = sentences[4];
-    
-    var line1cnt = line1.split('');
-    var line2cnt = line2.split('');
-    var line3cnt = line3.split('');
-    var line4cnt = line4.split('');
-    var line5cnt = line5.split('');
-        
+   
     $('#words').append(sentences[0]);
+    
+    var firstLtr = 0;
+    var firstLtrDisplay = sentences[0].charAt(firstLtr);
+    console.log(firstLtrDisplay);
     
     // above working thru array by line
     $('#keyboard-upper-container').hide();
@@ -61,5 +51,18 @@ $(document).ready(function(){
     });
 // end of original script tag below
 });   
-     
+        
+        // not used variables below. Spitting the lines of the array, and counting each character.
+        // var letterCount = 0; //setting at zero before keyUp or keyPress
+        // var currentLetter;    
+        // var line1 = sentences[0];
+        // var line2 = sentences[1];
+        // var line3 = sentences[2];
+        // var line4 = sentences[3];        
+        // var line5 = sentences[4];
 
+        // var line1cnt = line1.split('');
+        // var line2cnt = line2.split('');
+        // var line3cnt = line3.split('');
+        // var line4cnt = line4.split('');
+        // var line5cnt = line5.split('');

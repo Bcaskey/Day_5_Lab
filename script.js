226a20843618fb22802207d9efe5e35540b0f0a5
Bcaskey <bcaskeyu@charter.net>
2016-03-02 09:13:42 -0600

    Moving local var to global

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// Set the lower case keyboard onload
 $(document).ready(function(){
-
-    // below working thru array by line
-    // working on going through a loop on top of a loop
     var sentences = [
         "ten ate neite ate nee enet ite ate inet ent eate", 
         "Too ato too nOt enot one totA not anot tOO aNot",
@@ -10,19 +6,20 @@ $(document).ready(function(){
         "itant eate anot eat nato inate eat anot tain eat",
         "nee ene ate ite tent tiet ent ine ene ete ene ate"
         ];
-   
-    $('#words').append(sentences[0]);
-    
+        
+    var keyCodePress;
+    var letter;
+    var letterCount = 0;
     var firstLtr = 0;
     var firstLtrDisplay = sentences[0].charAt(firstLtr);
-    console.log(firstLtrDisplay);
+    var keyCodeUp;
     
-    // above working thru array by line
+    $('#words').append(sentences[0]);
     $('#keyboard-upper-container').hide();
     
     $(document).on('keypress', function(e){
-        var keyCodePress = e.which;
-        var letter = (String.fromCharCode(keyCodePress));
+        keyCodePress = e.which; // var moved to top of page
+        letter = (String.fromCharCode(keyCodePress)); //var moved to top
         $('#words-typed').append(letter);
         $('#next-letter').text(letter);
             //these are regular char
@@ -32,7 +29,11 @@ $(document).ready(function(){
             } else {
                 $('#' + keyCodePress).addClass('keyPressed');
             }
+            $('#block').prepend('&nbsp;').html;
     });
+    
+   
+    
     // When shift key is down, show the upper case keyboard
     $(document).keydown(function(e){ //keydown understands the key, but not the optional values of the key shift/noshift
         if (e.which == 16) {
@@ -42,27 +43,25 @@ $(document).ready(function(){
     });
     // when shift key is up, go back to lower case keyboard
     $(document).keyup(function (e){
-        var keyCodeUp = e.which;
+        keyCodeUp = e.which; //var moved to top
         $('*').removeClass('keyPressed');
             if (e.which == 16) {
                 $("#keyboard-lower-container").show();
                 $("#keyboard-upper-container").hide();
-        }
-    });
+            } 
+        // var testSent = [
+        // "ten ate neite ate nee enet ite ate inet ent eate Too ato too nOt enot one totA not anot tOO aNot oat itain oat tain nate eate tea anne inant nean itant eate anot eat nato inate eat anot tain eat nee ene ate ite tent tiet ent ine ene ete ene ate"
+        // ];
+        // $(testSent).each(function(){
+        //     var letters = this.split('');
+        //     console.log(letters);
+        //     //var eachLetter = testSent.length;
+        //     //for (var i = 0; i< eachLetter; i++) {
+        //     //console.log('z' + testSent[i]);
+        //     }
+
+        }); 
+    //});
 // end of original script tag below
 });   
-        
-        // not used variables below. Spitting the lines of the array, and counting each character.
-        // var letterCount = 0; //setting at zero before keyUp or keyPress
-        // var currentLetter;    
-        // var line1 = sentences[0];
-        // var line2 = sentences[1];
-        // var line3 = sentences[2];
-        // var line4 = sentences[3];        
-        // var line5 = sentences[4];
 
-        // var line1cnt = line1.split('');
-        // var line2cnt = line2.split('');
-        // var line3cnt = line3.split('');
-        // var line4cnt = line4.split('');
-        // var line5cnt = line5.split('');

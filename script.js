1bd727a518aeea781e0a86c64809f669ef8f9139
Bcaskey <bcaskeyu@charter.net>
2016-03-11 09:16:49 -0600

    added ltr count, moving box and message

--- script.js
+++ script.js
@@ -1,67 +1,119 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    $('#keyboard-upper-container').hide();
+
     var sentences = [
-        "ten ate neite ate nee enet ite ate inet ent eate", 
+        "ten ate neite ate nee enet ite ate inet ent eate",
         "Too ato too nOt enot one totA not anot tOO aNot",
         "oat itain oat tain nate eate tea anne inant nean",
         "itant eate anot eat nato inate eat anot tain eat",
         "nee ene ate ite tent tiet ent ine ene ete ene ate"
-        ];
-        
-    var keyCodePress;
-    var letter;
+    ];
+
+    var keyCodePress;//ASCII for which key was pressed
+    //var letter;
     var letterCount = 0;
     var firstLtr = 0;
     var firstLtrDisplay = sentences[0].charAt(firstLtr);
     var keyCodeUp;
+    var sentenceIndex = 0;
+    var LetterIndex = 0; //the first letter of the indexed sentence.
+    var errors = 0;
+    var sentence;
+    var time;
+
+    function setWordText(event) {
+        if (sentenceIndex < sentences.length) {//loops thru array looking for all values used
+            sentence = sentences[sentenceIndex];//define what sentence u are using
+            letterIndex = 0; //has to be zero again here, for loop statement
+            sentenceIndex++;//counting through sentence array items.
+            $('#words').text(sentence);
+            setNextLetterDiv(sentence[letterIndex]);//this sets the letterIndex value to the next letter
+            var numErrors = $('#words-typed').find('span.glyphicon-remove').length;// append div
+            errors += numErrors; // counts
+            $('#words-typed').empty();//IS THIS A FUNCTION?
+            $('#block').stop(true, true).animate({ 'left': '15px' });//WHAT ARE THE TRUE VALUES? AND WHY STOP ANIMATE?
+        } else {
+            //After doing all above, we are done. Play again, or stop?
+            var wpm = calculateWPM(event);//calls the calculate funciton, declare wpm
+            displayScore(wpm);//passes display score, with a value;
+            askToPlayAgain();//calls play again
+        }
+    };
+
+    setWordText();//calls all the if loop for sentences
     
-    $('#words').append(sentences[0]);
-    $('#keyboard-upper-container').hide();
-    
-    $(document).on('keypress', function(e){
-        keyCodePress = e.which; // var moved to top of page
-        letter = (String.fromCharCode(keyCodePress)); //var moved to top
-        $('#words-typed').append(letter);
-        $('#next-letter').text(letter);
-            //these are regular char
-            if ((keyCodePress >=0) && (keyCodePress < 32) || (keyCodePress > 47) && (keyCodePress < 58) || (keyCodePress > 64) && (keyCodePress < 91) || (keyCodePress > 96) && (keyCodePress < 123)) {
-                $('#' + letter).addClass('keyPressed');
-            //these are for spec char
-            } else {
-                $('#' + keyCodePress).addClass('keyPressed');
+    function calculateWPM(event) {
+        time = event.timeStamp - time; //time stamp, less time
+        var mistakes = errors;
+        var numberOfWords = 54;
+        var minutes = (time / 1000) / 60;
+        var wpm = Math.floor(numberOfWords / minutes - (mistakes * 2));
+        return wpm;
+    };
+
+    function displayScore(wpm) {
+        var message;
+        if (parseInt(wpm) < 0) {
+            message = 'There were a lot of mistakes!';
+        } else {
+            message = 'You typed ' + wpm + ' words per minute!';
+        }
+        $('#message').html(message).css({ 'font-size': '30px' });
+    };
+
+    function askToPlayAgain() {
+        setTimeout(function() { // Built into JS.
+            if (confirm('Would you like to play again?')) {
+                sentenceIndex = 0;
+                time = undefined;
+                $('#message').empty();
+                setWordText();
             }
-            $('#block').prepend('&nbsp;').html;
+        }, 5000);
+    }
+
+    function setNextLetterDiv(letterIndex) { //changed from letter as example
+        $('#next-letter').text(letterIndex); //changed from letter as example
+    }
+    function moveBlock() {
+        $("#block").animate({ "left": "+=17px" }, 80);
+    }
+
+    $(document).on('keypress', function(e) {
+        if (!time) { // setting time start
+            time = e.timeStamp;
+        }
+        
+        var keyCodePress = e.keyCode;        
+        if (keyCodePress === sentence[letterIndex].charCodeAt()) {
+            $('#words-typed').append($('<span class="glyphicon glyphicon-ok"></span>'));
+        } else {
+            $('#words-typed').append($('<span class="glyphicon glyphicon-remove"></span>'));
+        }
+        letterIndex++;
+        if (letterIndex < sentence.length) { // Still letters remaining in current sentence
+            moveBlock();
+            setNextLetterDiv(sentence[letterIndex]);
+        } else { // End of the sentence. Call our setWordText setup function
+            setWordText(e); // Finishing things up for this sentence
+        }
     });
-    
-   
-    
-    // When shift key is down, show the upper case keyboard
-    $(document).keydown(function(e){ //keydown understands the key, but not the optional values of the key shift/noshift
+    var selector = '#' + keyCodePress;
+    $(selector).css({ 'background-color': '#38B0DE', 'color': '#fff' });
+
+    $(document).keydown(function(e) {
         if (e.which == 16) {
             $("#keyboard-upper-container").show();
             $("#keyboard-lower-container").hide();
         }
     });
     // when shift key is up, go back to lower case keyboard
-    $(document).keyup(function (e){
+    $(document).keyup(function(e) {
         keyCodeUp = e.which; //var moved to top
         $('*').removeClass('keyPressed');
-            if (e.which == 16) {
-                $("#keyboard-lower-container").show();
-                $("#keyboard-upper-container").hide();
-            } 
-        // var testSent = [
-        // "ten ate neite ate nee enet ite ate inet ent eate Too ato too nOt enot one totA not anot tOO aNot oat itain oat tain nate eate tea anne inant nean itant eate anot eat nato inate eat anot tain eat nee ene ate ite tent tiet ent ine ene ete ene ate"
-        // ];
-        // $(testSent).each(function(){
-        //     var letters = this.split('');
-        //     console.log(letters);
-        //     //var eachLetter = testSent.length;
-        //     //for (var i = 0; i< eachLetter; i++) {
-        //     //console.log('z' + testSent[i]);
-        //     }
-
-        }); 
-    //});
-// end of original script tag below
+        if (e.which == 16) {
+            $("#keyboard-lower-container").show();
+            $("#keyboard-upper-container").hide();
+        }
+    });
 });   
-
